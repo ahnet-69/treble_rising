7777ccdb4b2946f4d4cd6715b4eb798ba5b84674
ahnet-69 <syedmusicrealise@gmail.com>
2023-05-20 18:02:27 +0500

    Remove useless configs breaking systemui and more

--- RisingOS.mk
+++ RisingOS.mk
@@ -1,9 +1,5 @@
-$(call inherit-product, vendor/lineage/config/common_full.mk)
-$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
-$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
-$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
--include vendor/lineage/build/core/config.mk
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
