955ee7334538624fc0f1d687f446cf8eb39da8d7
ahnet-69 <syedmusicrealise@gmail.com>
2023-05-21 12:33:28 +0500

    Add tablet configs

--- RisingOS.mk
+++ RisingOS.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
